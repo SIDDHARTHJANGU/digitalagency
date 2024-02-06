0651592013cb21c3d75347d1c75f475a0d6863c7
SIDDHARTHJANGU <siddharthbishnoi1322@gmail.com>
2024-02-06 09:32:15 +0530

    all

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import Hero from "./Components/Hero";
 // import NewPage from "./Components/NewPage";
-// import { Myheader } from "./Components/Myheader";
-// import Features from "./Components/Features";
-// import OurProcess from "./Components/OurProcess";
-// import Brands from "./Components/Brands";
-// import Myfooter from "./Components/Myfooter";
-// import StartWorking from "./Components/StartWorking";
+import { Myheader } from "./Components/Myheader";
+import Features from "./Components/Features";
+import OurProcess from "./Components/OurProcess";
+import Brands from "./Components/Brands";
+import Myfooter from "./Components/Myfooter";
+import StartWorking from "./Components/StartWorking";
 
 // import About from "./Components/About";
 
 function App() {
   return (
     <div className=" sec_twobg overflow-hidden">
-      
-      {/* <Myheader />
+      <Myheader />
       <Features />
       <OurProcess />
       <Brands />
       <StartWorking />
-      <Myfooter /> */}
+      <Myfooter />
       {/* <NewPage />
       <Hero />
       <About /> */}
